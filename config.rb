83da774d0e3ba7d012fa9d8a42a4039eb9c4e1a7
mb_kondou <kondou@maboroshi.biz>
2014-11-16 05:59:19 +0900

    フォルダ移動

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/"
-sass_dir = "/"
+css_dir = "css"
+sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 preferred_syntax = :sass
