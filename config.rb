05a84fb969b3a4d0dcdf537e9a8e77330cc76e40
mb_kondou <kondou@maboroshi.biz>
2014-11-16 05:50:09 +0900

    first commit

---
+++ config.rb
@@ -0,0 +1,42 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "/"
+images_dir = "images"
+javascripts_dir = "javascripts"
+preferred_syntax = :sass
+output_style = :expanded
+line_comments = false
+relative_assets = true
+cache = false
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+require 'sass'
+
+module Sass::Script
+  class Color
+    def to_s(opts = {})
+      return rgba_str if alpha?
+      return smallest# if options[:style] == :compressed
+      #return COLOR_NAMES_REVERSE[rgb] if COLOR_NAMES_REVERSE[rgb]
+      hex_str
+    end
+  end
+end
---
+++ jquery.swipeList.js
@@ -0,0 +1,96 @@
+(function($){
+  $.fn.swipeList = function(op){
+    /**
+     * @prop targetEle {element} [.js-swipeListTarget] スワイプターゲット要素
+     * @prop btnEle {element} ボタン親要素
+     * @prop speed {number} アニメーションスピード
+     * @prop direction {string} スワイプ方向
+    */
+    op = $.extend({
+      targetEle : ".js-swipeListTarget",
+      btnEle : ".js-swipeListBtn",
+      speed: 300,
+      direction: "left"
+    },op);
+
+    return this.each(function(){
+      var $this = $(this);
+      ({
+        /**
+         * 初期設定 イベント登録
+        */
+        init: function(){
+          $this.find(op.targetEle).on("touchstart", this.swipeStert);
+          $this.find(op.targetEle).on("touchmove", this.swipeMove);
+          $this.find(op.targetEle).on("touchend", this.swipeEnd);
+        },
+        /**
+         * touchstartイベント
+         * @prop start {number} 開始位置
+         * @param e {events} イベントオブジェクト
+        */
+        swipeStert: function(e){
+          this.start = e.originalEvent.touches[0].pageX;
+          $this.find(op.targetEle).css("transition", "all 1ms ease");
+          if(this.flagMove){
+            $this.find(op.targetEle).css("transition", "all " + op.speed + "ms ease");
+            $this.find(op.targetEle).css("transform", "translate3d(0, 0, 0)");
+            this.move = 0;
+            this.moveVal = 0;
+            this.flagMove = false;
+            return false;
+          }
+        },
+        /**
+         * touchmoveイベント
+         * @prop move {number} 移動距離
+         * @prop moveVal {number} 開始位置 - 移動距離
+         * @param e {events} イベントオブジェクト
+        */
+        swipeMove: function(e){
+          this.move = e.originalEvent.touches[0].pageX;
+          this.moveVal = this.start - this.move;
+          if(op.direction == "left"){
+            if(this.moveVal > 5){
+              e.preventDefault();
+            }
+            if(this.moveVal > 0){
+              $this.find(op.targetEle).css("transform", "translate3d(-" + this.moveVal + "px, 0, 0)");
+            }
+          }else{
+            if(this.moveVal < 5){
+              e.preventDefault();
+            }
+            if(this.moveVal < 0){
+              $this.find(op.targetEle).css("transform", "translate3d(" + Math.abs(this.moveVal) + "px, 0, 0)");
+            }
+          }
+        },
+        /**
+         * touchendイベント
+         * @prop baseVal {number} 目標までの移動距離 (ボタン幅)
+         * @param e {events} イベントオブジェクト
+        */
+        swipeEnd: function(e){
+          this.baseVal = $this.find(op.btnEle).innerWidth();
+          $this.find(op.targetEle).css("transition", "all " + op.speed + "ms ease");
+          if(op.direction == "left"){
+            if(this.moveVal > 60){
+              $this.find(op.targetEle).css("transform", "translate3d(-" + this.baseVal + "px, 0, 0)");
+              this.flagMove = true;
+            }else{
+              $this.find(op.targetEle).css("transform", "translate3d(0, 0, 0)");
+            }
+          }else{
+            if(this.moveVal < 60){
+              $this.find(op.targetEle).css("transform", "translate3d(" + Math.abs(this.baseVal) + "px, 0, 0)");
+              this.flagMove = true;
+            }else{
+              $this.find(op.targetEle).css("transform", "translate3d(0, 0, 0)");
+            }
+          }
+        }
+      }).init();
+    });
+  }
+})(jQuery);
